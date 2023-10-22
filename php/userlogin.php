1fef62bfe796446473ea9871bbdd0fd6ba61936a
IanPrinceD <princedumanhug@gmail.com>
2023-10-22 21:28:28 +0800

    creation and designing of login and signup page

--- php/userlogin.php
+++ php/userlogin.php
@@ -2,9 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- bootstrap link -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- favicon -->
+	<link rel="icon" type="image/x-icon" href="../images/PNG/Neoeye Optical Clinic Logo.png">
+
+	<!-- bootstrap link -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -17,14 +21,33 @@
 	<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 
 	<!-- css link -->
-    <title>Login Users</title>
+	<link rel="stylesheet" href="../css/login.css">
+	<link rel="stylesheet" href="../css/index.css">
+	<title>Login Users</title>
 </head>
 
 <body>
-    <form action="" method="post">
-
-
-    </form>
+	<main class="adminlogin">
+		<section class="form-section">
+			<img class="img-logo" src="../images/PNG/Neoeye Optical Clinic Logo.png" alt="Neoeye Optical Clinic Logo">
+			<h5>Neoeye Optical Clinic</h5>
+			<h1>Welcome</h1>
+			<form method="POST">
+				<input class="username" type="text" name="name" placeholder="Username">
+				<input class="password" type="password" name="pass" placeholder="Password">
+				<div class="btn-logs">
+					<input class="btn login-btn" type="submit" name="Login" value="LOGIN">
+					<a class="btn cancel-btn" href="../index.php" role="button">CANCEL</a>
+				</div>
+				<a class="link" href="#">Forgot password?</a>
+			</form>
+			<hr>
+			<p class="signup">Don't have an account? <a class="link" href="#">Sign Up</a></p>
+		</section>
+		<section class="img-section">
+			<img class="img-gif" src="#" alt="anything related in clinic">
+		</section>
+	</main>
 </body>
 
 </html>
